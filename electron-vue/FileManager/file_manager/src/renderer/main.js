07307642a360114ed492d7a66986d729cc8e612e
zjtomoon <zjtomoon@outlook.com>
2022-04-07 13:52:52 +0800

    default commit

--- electron-vue/FileManager/file_manager/src/renderer/main.js
+++ electron-vue/FileManager/file_manager/src/renderer/main.js
@@ -5,6 +5,12 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+// use antd-vue
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Antd)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
---
+++ electron-vue/readme.md
@@ -0,0 +1,14 @@
+# electron-vue开发
+
+## 创建项目
+
+[创建项目](./docs/%E5%88%9B%E5%BB%BA%E9%A1%B9%E7%9B%AE.md)
+
+## 打包注意事项
+
+[打包注意事项](./docs/%E6%89%93%E5%8C%85%E6%B3%A8%E6%84%8F%E4%BA%8B%E9%A1%B9.md)
+
+### build.js修改
+[build.js](./docs/build.js)
+
+[package.json](./docs/package.json)
