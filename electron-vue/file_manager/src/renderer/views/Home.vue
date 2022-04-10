ef6301bf187a447a113d5065d014aed179cb311d
zjtomoon <zjtomoon@outlook.com>
2022-04-10 17:23:01 +0800

    file_manager

--- electron-vue/file_manager/src/renderer/views/Home.vue
+++ electron-vue/file_manager/src/renderer/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
         <h1>Hello,electron-vue!</h1>
+        <FileUpload/>
     </div>
 </template>
 
 <script>
+    import FileUpload from "./FileUpload";
     export default {
-        name: "Home"
+        name: "Home",
+        components: {FileUpload}
     }
 </script>
 
